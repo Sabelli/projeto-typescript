8d0eb98a206841f808b4c7c9701021a2bed87d5a
Sabelli <sabellinho@gmail.com>
2026-02-25 20:11:00 -0300

    feature: importando tipos no index.ts

---
+++ sistema-consultas/src/index.ts
@@ -0,0 +1,5 @@
+import type { Especialidade } from "./types/especialidade";
+import type { Paciente } from "./types/paciente";
+import type { StatusConsulta } from "./types/statusConsulta";
+import type { Medico } from "./interfaces/medico";
+import type { Consulta } from "./interfaces/consulta";
